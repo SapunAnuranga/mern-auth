dd042b7ca11da0dbdd13f123085360c8f1d60f94
SAPUN ANURANGA <sapunanuranga98@gmail.com>
2025-04-20 12:33:51 +0530

    Created auth routes and controller for register, login, and logout.

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config';
 import cookieParser from "cookie-parser";
 
 import connectDB from './config/mongodb.js';
+import authRoutes from './routes/authRoutes.js';
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -14,5 +15,6 @@ app.use(cookieParser());
 app.use(cors({ credentials: true, origin: true }));
 
 app.get('/', (req, res) => res.send("API WORKING"));
+app.use('/api/auth', authRoutes);
 
 app.listen(port, () => console.log(`Server Started on PORT: ${port}`));
